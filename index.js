4978afd52d5ef3e218de75d242bc58ff6e58d8ec
demetriotorgan <demetriotorgan@hotmail.com>
2025-11-24 14:32:18 -0300

    semHeartBeat

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use((req,res, next)=>{
 connectDB();
 
 // 🔥 Iniciar heartbeat (manter conexão viva)
-iniciarHeartbeat(); // <-- aqui está o segredo!
+// iniciarHeartbeat(); 
 
 app.use('/', trechoRoutes);
 app.get('/', (req, res)=>{;
